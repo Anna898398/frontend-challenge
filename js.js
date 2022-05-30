37f000502d4122f3f3f53999e0ba2aa5aa37997f
Студент ТОР Академии <std@itstep.loc>
2022-05-30 21:10:05 +0300

    верстка

--- js.js
+++ js.js
@@ -4,7 +4,7 @@
 
 
 async function getAllCAts(){
-    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=15',{
+    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=25',{
 headers:{
     "x-api-key":"48ef3a50-cb8a-4f11-807d-e39a84e58f59"
 }
